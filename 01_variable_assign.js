0c8f1f7bd2d5839d4708b9e96c88bf9425ffae0c
Monika <monikapawar538@gmail.com>
2022-12-08 11:39:39 +0530

    variable declaration and initialization in first page
    variable declaration and initialization in first page

---
+++ 01_variable_assign.js
@@ -0,0 +1,12 @@
+var myFullName;
+myFullName="Prabhanjan Pawar";
+console.log(myFullName);
+
+var myFullName="Prabhanjan Arun Pawar";
+console.log(myFullName);
+
+var myLove="I Love Only JS";
+console.log(myLove);
+
+var firstName="Prabhanjan", latsName="Pawar", age="30", address="Pimple Gurav,Pune", pin_code="411061", gender="Male", is_married="Yes";
+console.log(firstName, latsName, age, address, pin_code, gender, is_married);
---
+++ hello.js
@@ -0,0 +1,17 @@
+// This is my first java code
+console.log("Hello Mohit");
+
+// variable declaration;
+var myName;    
+
+// value initialisation;
+myName="Prabhanjan";
+console.log(myName);
+
+// value Override;
+myName="Prabhanjan Pawar";
+console.log(myName);
+
+// variable declaration with value initilisation;
+var myFullName="Prabhanjan Arun Pawar"
+console.log(myFullName);
