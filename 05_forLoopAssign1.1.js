27f199f42ff737189a94133c6dd3b4f7e3267b31
Monika <monikapawar538@gmail.com>
2022-12-20 16:44:57 +0530

    for loop to print 5 to 15 using for Loop
    for loop to print 5 to 15 using for Loop

---
+++ 05_forLoopAssign1.1.js
@@ -0,0 +1,54 @@
+console.log(`*****"For Loop : to print numbers from 5 to 15 incrementing by 1"*****\n`);
+
+function forLoopOne(number) {
+    
+    var num;
+    for (let num = 5; num <= 15; num++) {
+        //const element = array[index];
+        console.log(num);
+    }
+}
+forLoopOne();
+
+// console.log(`*****"For Loop : to print numbers from 50 to 40 decrementing by 1"*****\n`);
+
+// function forLoopTwo(number1) {
+    
+//     var num1;
+//     for (let num1 = 50; num1 >= 40; num1--) {
+//         console.log(num1);
+//     }
+// }forLoopTwo();
+
+// console.log(`*****"For Loop : to find First 15 ODD numbers"*****\n`);
+
+// function forLoopOdd(number2) {
+ 
+//     var num2;
+//     var temp=0;
+//     for (let num2 = 1; num2 < 30; num2++) {
+        
+//         if (num2%2!=0) {
+//             console.log(num2);
+//         }      
+//         // temp=num2;
+//         // num2++;
+//         // console.log(temp);
+//     }   
+// }forLoopOdd();
+
+// console.log(`*****"For Loop : to find First 10 EVEN numbers"*****\n`);
+
+// function forLoopEven(number3) {
+ 
+//     var num3;
+//     for (let num3 = 1; num3 < 20; num3++) {
+        
+//         if (num3%2==0) {
+//             console.log(num3);
+//         }      
+//         // temp=num2;
+//         // num2++;
+//         // console.log(temp);
+//     }   
+// }forLoopEven();
