65058ec171a38d7835da4b5bdc766c40f4420230
Monika <monikapawar538@gmail.com>
2022-12-21 14:44:27 +0530

    while loop to print no from 50to40 by decrementing 1
    while loop to print no from 50to40 by decrementing 1

--- 05_whileAndDoWhile.js
+++ 05_whileAndDoWhile.js
@@ -1,6 +1,13 @@
-console.log(`" ***** While Loop ***** "\n`);
-var i=5;
-while (i<=15) {
-    console.log(i);
-    i++;
+console.log(`" ***** While Loop to print Numbers from 5 to 15 by Incrementing 1***** "\n`);
+var i=5;                //Initialization/declatartion;
+while (i<=15) {         //Condition
+    console.log(i); 
+    i++;                //Update Expression
+}
+
+console.log(`" ***** While Loop to print numbers from 50 to 40 by Decrementing 1***** "\n`);
+var a=50;               //Initialization/declatartion;
+while (a>=40) {         //Condition
+    console.log(a);     
+    a--;                //Update Expression
 }
