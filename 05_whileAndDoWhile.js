cbb1ff798d4c0122620335cd27b37ce87291caa5
Monika <monikapawar538@gmail.com>
2022-12-21 15:44:26 +0530

    while to print first 10 EVEN numbers
    while to print first 10 EVEN numbers

--- 05_whileAndDoWhile.js
+++ 05_whileAndDoWhile.js
@@ -1,23 +1,23 @@
-console.log(`" ***** While Loop to print Numbers from 5 to 15 by Incrementing 1***** "\n`);
+console.log(`\n" ***** While Loop to print Numbers from 5 to 15 by Incrementing 1***** "\n`);
 var i=5;                //Initialization/declatartion;
 while (i<=15) {         //Condition
     console.log(i); 
     i++;                //Update Expression
 }
 
-console.log(`" ***** While Loop to print numbers from 50 to 40 by Decrementing 1***** "\n`);
+console.log(`\n" ***** While Loop to print numbers from 50 to 40 by Decrementing 1***** "\n`);
 var a=50;               //Initialization/declatartion;
 while (a>=40) {         //Condition
     console.log(a);     
     a--;                //Update Expression
 }
 
-console.log(`" ***** While Loop to find 15 first ODD Numbers***** "\n`);
+console.log(`\n" ***** While Loop to find 15 first ODD Numbers***** "\n`);
 
 var b=1;
 var counter=0;
 while (b<=100) {
-    if (b%2==0) {
+    if (b%2!=0) {
         console.log(b);        
         counter=counter+1;
     }
@@ -25,5 +25,21 @@ while (b<=100) {
         break;
     }
     b++;
+}  
+console.log(`Total no of ODD numbers : ${counter}`);
+
+console.log(`\n" ***** While Loop to find 10 first EVEN Numbers***** "\n`);
+
+var d=1;
+var counter=0;
+while (d<=100) {
+    if (d%2==0) {
+        console.log(d);        
+        counter=counter+1;
+    }
+    if (counter==10) {
+        break;
+    }
+    d++;
 }  
 console.log(`Total no of EVEN numbers : ${counter}`);
