08056f31392717b0154b9f2121b76ff687e4dc5c
Monika <monikapawar538@gmail.com>
2022-12-22 14:39:35 +0530

    updated data
    updated data

--- 05_marriageEligibility.js
+++ 05_marriageEligibility.js
@@ -1,30 +1,26 @@
 console.log(`---------"This is Marriage Eligibility Check"----------`);
 
 function marriageEligibility(gender, age) {
-    
-    if (gender=="Female" || gender=="Male") {
-        
-        if (age>=21 && gender=="Male") {
-            console.log(`Age - ${age} : Hey Man, "You are eligible for Marriage"`);
-            return;
-        } 
-            if(age>=18 && gender=="Female") {
-                console.log(`Age - ${age} : Hey Lady, "You are eligible for Marriage"`);
-                return;
-            }
-                if(age)
-                    {
-                        console.log(`Age - ${age} : "Sorry you are not Eligible for Marriage"`);
-                     }
-    }
-    else{
-        console.log(`Gender : "${gender}", is not valid gender, Please provide valid details`);
-    }
-}
-marriageEligibility("Male",17);
-marriageEligibility("Male",25);
-marriageEligibility("Female",16);
-marriageEligibility("Female",28);
-marriageEligibility("Other",35);
-//marriageEligibility("Other",41);
+  if (gender == "Female" || gender == "Male") {
 
+    if (age >= 21 && gender == "Male" || age >= 18 && gender == "Female") {
+      console.log(`Hey ${gender} your age is ${age} : "You are eligible for Marriage"`);
+      return;
+    }
+    // if (age >= 18 && gender == "Female") {
+    //   console.log(`Age - ${age} : Hey Lady, "You are eligible for Marriage"`);
+    //   return;
+    // }
+    else {
+      console.log(`Hey ${gender} your age is ${age} : "Sorry you are not Eligible for Marriage"`);
+    }
+  } else {
+    console.log(`Gender : "${gender}", is not valid gender, Please provide valid details`);
+  }
+}
+marriageEligibility("Male", 17);
+marriageEligibility("Male", 25);
+marriageEligibility("Female", 16);
+marriageEligibility("Female", 28);
+marriageEligibility("Other", 35);
+//marriageEligibility("Other",41);
