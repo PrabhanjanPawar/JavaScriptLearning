33934c3bee7cb71b2e5eb04c65a49651c084c91b
Monika <monikapawar538@gmail.com>
2022-12-27 21:21:05 +0530

    Basics of Array concepts
    Basics of Array concepts

---
+++ 07_arrayBasic.js
@@ -0,0 +1,36 @@
+let arrayOfNumbers = [5, 10, 4, 6, 7, 10, 6]; // Empty Array
+console.log(arrayOfNumbers);
+
+console.log(`===== Total number of elements available in array ====`);
+let totalElements = arrayOfNumbers.length;
+console.log(`Total Elements in arrayOfNumbers --> ${totalElements}`);
+
+let typeOfArray =typeof arrayOfNumbers;
+console.log(`Type of array - arrayOfNumbers --> ${typeOfArray}`);
+
+console.log(`=== Accessing array elements===`);
+let elementAtZeroIndex = arrayOfNumbers[0];
+console.log(`Element at Zero Index : ${elementAtZeroIndex}`);
+console.log(`Element at Fifth Index : ${arrayOfNumbers[5]}`);
+
+console.log(`Last Element : ${totalElements-1}`);
+
+console.log(`When provided value is greater than length : ${arrayOfNumbers[9]}`);
+
+arrayOfNumbers[2] = 100; // Update or Modify value using index
+console.log(arrayOfNumbers);
+
+console.log(`Value at 4th position is : ${arrayOfNumbers[4]}`);
+console.log(`Replacing value of 4th positon by 12 : ${arrayOfNumbers[4]=12}`);
+console.log((arrayOfNumbers));
+
+const pos=arrayOfNumbers.indexOf(4);
+console.log(pos);
+
+console.log(`======== splice() methods=========`);
+var arrayOfNum = [4, 5, 6, 8, 9, 33, 66];
+console.log(arrayOfNum);
+let spliceResult = arrayOfNum.splice(3);
+console.log(`After using splice(3) method `);
+console.log("Array after deleting elements ", arrayOfNum);
+console.log("Total deleted elements: ", spliceResult);
